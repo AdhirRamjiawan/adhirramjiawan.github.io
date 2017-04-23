9e02dfeeafe920a58c13cf278167fbe8e9c9ea5b
Adhir Ramjiawan <adhirramjiawan0@gmail.com>
2017-04-23 03:20:25 +0200

    added zoom

--- ludum-dare-38/scripts/game.js
+++ ludum-dare-38/scripts/game.js
@@ -1,16 +1,16 @@
 window.addEventListener("load",function() {
   var Q = window.Q = Quintus({development: true})
     .include("Scenes, Sprites, 2D, Input, Touch, UI, TMX, Audio")
-    //.include("ActionPlatformerPlayer")
     .setup({
       width: 640,   //to fit devices with a screne resolution of 1280 x 720
       height: 480
     }).controls().touch().enableSound();
 
     Q.setImageSmoothing(false);
-
+    
     Q.scene('level', function(stage) {
       var player;
+      var world = new Q.World();
 
       player = Q("Player").first();
       
@@ -28,7 +28,33 @@ window.addEventListener("load",function() {
             y: 300
         }));
 
-      stage.insert(new Q.World());
+        Q.input.on('left',stage,function(e) { 
+            console.log("input!");
+
+            if (world.p.scale > 1) {
+                world.p.scale -= 0.1;
+                world.p.cx += 100;
+                world.p.cy += 100;
+
+                world.p.x += 100;
+                world.p.y += 100;
+            }
+            
+        });
+
+        Q.input.on('right',stage,function(e) { 
+            console.log("input!");
+            world.p.scale += 0.1;
+            world.p.cx -= 100;
+            world.p.cy -= 100;
+            
+            world.p.x -= 100;
+            world.p.y -= 100;
+
+            console.log(world.p);
+        });
+
+      stage.insert(world);
     });
 
     Q.load([ "Ludum dare 38 loop 2.mp3" ], function() {
@@ -41,11 +67,13 @@ window.addEventListener("load",function() {
                 this._super(p, {
                     asset: "earth.png",
                     x: 450,
-                    y: 300
+                    y: 300,
+                    scale: 1
                 });
 
-                this.add("platformerControls");
                 console.log(this);
+            },
+            step: function() {
             }
         });
 
